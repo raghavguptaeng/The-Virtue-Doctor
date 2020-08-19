99cece7ca30641fbb1167cb0d26570bc02ea4d7e
raghav gupta <raghavguptaeng@users.noreply.github.com>
2020-08-19 20:34:24 +0530

    I made a contact window

--- Main.py
+++ Main.py
@@ -3,4 +3,13 @@ app = Flask(__name__)
 @app.route('/')
 def HomePage():
     return render_template('index.html')
+
+@app.route('/Contact.html')
+def Develouper_contact():
+    return render_template('Contact.html')    
+
+@app.route('/index.html')
+def Home_back():
+    return render_template('index.html')
+
 app.run(debug = True)    
