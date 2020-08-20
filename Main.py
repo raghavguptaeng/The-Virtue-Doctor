fd8066792ac477c9de3f07893df4ed45998bdce4
raghav gupta <raghavguptaeng@users.noreply.github.com>
2020-08-20 23:21:39 +0530

    Database connected

--- Main.py
+++ Main.py
@@ -1,15 +1,54 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+import json
+#-----------------------------------------------------------------
 app = Flask(__name__)
-@app.route('/')
+with open('info.json','r') as c:
+    info = json.load(c)["Information"]
+
+app.config['SQLALCHEMY_DATABASE_URI'] = info["Production_Server"] 
+db = SQLAlchemy(app)   
+#-----------------------------------------------------------------
+class Feedback(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(100),  nullable=False )
+    Email = db.Column(db.String(100),  nullable=False)
+    Message = db.Column(db.String(1000),  nullable=False)
+#-----------------------------------------------------------------  
+class Subscribers(db.Model):
+    Email = db.Column(db.String(100),  primary_key=True)
+#-----------------------------------------------------------------   
+@app.route('/', methods=['GET','POST'])
 def HomePage():
+    if(request.method == 'POST'):
+        email = request.form.get('email')
+        en = Subscribers(Email = email)
+        db.session.add(en)
+        db.session.commit()
     return render_template('index.html')
-
-@app.route('/Contact.html')
+#-----------------------------------------------------------------
+@app.route('/Contact.html' ,  methods=['GET','POST'])
 def Develouper_contact():
+
+    if(request.method == 'POST'):
+        """ Add entry to the database """
+        name = request.form.get('Name')
+        email = request.form.get('Email')
+        message = request.form.get('Message')    
+        entry = Feedback(Name = name , Email = email , Message = message)
+        db.session.add(entry)
+        db.session.commit()    
+        
     return render_template('Contact.html')    
-
-@app.route('/index.html')
+#-----------------------------------------------------------------
+@app.route('/index.html', methods=['GET','POST'])
 def Home_back():
-    return render_template('index.html')
+    if(request.method == 'POST'):
+        email = request.form.get('email')
+        en = Subscribers(Email = email)
+        db.session.add(en)
+        db.session.commit()
 
+    return render_template('index.html')
+#-----------------------------------------------------------------
 app.run(debug = True)    
